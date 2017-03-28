34b4a8496d2cb3b44109f2767b7ec1fac93a2bb6
Federico Aloi <federico.aloi@gmail.com>
2017-03-28 19:00:44 -0300

    Updated extra.rb

--- extra.rb
+++ extra.rb
@@ -1,64 +1,64 @@
 module BuenosAires 
-  def kilometro; 0 end
+  def self.kilometro; 0 end
 end
 
 module SantaFe
-  def kilometro; 315 end
+  def self.kilometro; 315 end
 end
 
 module Rosario
-  def kilometro; 514 end
+  def self.kilometro; 514 end
 end
 
 module Mercedes
-  def cantar
+  def self.cantar
     "♪ una voz antigua de viento y de sal ♫"
   end
 end
 
 module Anastasia
-  def cantar
+  def self.cantar
     "priiiip priiiip"
   end
   
-  def volar_en_circulos; end
+  def self.volar_en_circulos; end
   
-  def comer_lombriz; end
+  def self.comer_lombriz; end
 end
 
 module Pepita
   @energia = 100
   @ciudad = Rosario
   
-  def energia=(una_energia) 
+  def self.energia=(una_energia) 
     @energia = una_energia
   end
   
-  def energia; @energia end
-  def ciudad; @ciudad end
+  def self.energia; @energia end
+  def self.ciudad; @ciudad end
   
-  def cantar
+  def self.cantar
     'pri pri pri'
   end
   
-  def comer_lombriz
+  def self.comer_lombriz
     @energia += 20
   end
   
-  def comer_alpiste(una_energia)
+  def self.comer_alpiste(una_energia)
     @energia += una_energia * 15
   end
   
-  def volar_en_circulos
+  def self.volar_en_circulos
     @energia -= 10
   end
   
-  def volar_hacia(una_ciudad)
+  def self.volar_hacia(una_ciudad)
     @energia -= self.distancia(una_ciudad) * 3
     @ciudad = una_ciudad
   end
   
-  def distancia(una_ciudad) 
+  def self.distancia(una_ciudad) 
     (@ciudad.kilometro - una_ciudad.kilometro).abs
   end
 end
