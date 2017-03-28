b14fdb2b014b804e3474bebf31b521dde9253d16
Federico Aloi <federico.aloi@gmail.com>
2017-03-28 19:04:30 -0300

    Updated extra.rb

--- extra.rb
+++ extra.rb
@@ -43,20 +43,23 @@ module Pepita
   
   def self.comer_lombriz
     @energia += 20
-    nil
+    return
   end
   
   def self.comer_alpiste(una_energia)
     @energia += una_energia * 15
+    return
   end
   
   def self.volar_en_circulos
     @energia -= 10
+    return
   end
   
   def self.volar_hacia(una_ciudad)
     @energia -= self.distancia(una_ciudad) * 3
     @ciudad = una_ciudad
+    return
   end
   
   def self.distancia(una_ciudad) 
