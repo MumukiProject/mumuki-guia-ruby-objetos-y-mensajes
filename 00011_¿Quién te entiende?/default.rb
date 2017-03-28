a6eb72c7ab443aae8ad27b9d5e9509afffac03f7
federico.aloi@gmail.com <federico.aloi@gmail.com>
2017-03-28 18:56:03 -0400

    Mumuki Export on 2017-03-28 18:56:03 -0400

---
+++ 00011_¿Quién te entiende?/default.rb
@@ -0,0 +1,14 @@
+interfaz_pepita = [
+    "energia", 
+    "cantar", 
+    "comerLombriz", 
+    "volarEnCirculos"
+]
+  
+interfaz_anastasia = [
+  #...completar acá...
+]
+  
+interfaz_mercedes = [
+  #...completar acá...
+}
---
+++ 00011_¿Quién te entiende?/test..rb
@@ -0,0 +1,21 @@
+#...extra...#
+
+describe 'Interfaz de' do
+  it 'Mercedes' do
+    #...content...#
+  
+    expect(interfaz_mercedes).to eq ['cantar']
+  end
+
+  it 'Pepita' do
+    #...content...#
+  
+    expect(interfaz_pepita).to eq ['energia', 'cantar', 'comer_lombriz', 'volar_en_circulos']
+  end
+
+  it 'Norita' do
+    #...content...#
+  
+    expect(interfaz_pepita).to eq ['cantar', 'comer_lombriz', 'volar_en_circulos']
+  end  
+end
