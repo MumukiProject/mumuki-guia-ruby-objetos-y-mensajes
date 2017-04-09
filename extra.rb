c4ff18d559bbe21b86ceddc771942afc57d0f3dc
Federico Aloi <federico.aloi@gmail.com>
2017-04-09 21:56:17 +0200

    Update extra.rb

--- extra.rb
+++ extra.rb
@@ -27,10 +27,10 @@ module Norita
     'priiiip priiiip'
   end
   
-  def self.volar_en_circulos
+  def self.volar_en_circulos!
   end
   
-  def self.comer_lombriz
+  def self.comer_lombriz!
   end
 end
 
@@ -54,22 +54,22 @@ module Pepita
     'pri pri pri'
   end
   
-  def self.comer_lombriz
+  def self.comer_lombriz!
     @energia += 20
     return
   end
   
-  def self.comer_alpiste(una_energia)
+  def self.comer_alpiste!(una_energia)
     @energia += una_energia * 15
     return
   end
   
-  def self.volar_en_circulos
+  def self.volar_en_circulos!
     @energia -= 10
     return
   end
   
-  def self.volar_hacia(una_ciudad)
+  def self.volar_hacia!(una_ciudad)
     @energia -= self.distancia(una_ciudad) * 3
     @ciudad = una_ciudad
     return
