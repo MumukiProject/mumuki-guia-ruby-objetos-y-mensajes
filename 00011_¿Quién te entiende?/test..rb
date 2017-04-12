a21c5c6757c8beb6d4c7b3825519ae61ac1324fb
federico.aloi@gmail.com <federico.aloi@gmail.com>
2017-04-12 09:43:37 -0400

    Mumuki Export on 2017-04-12 09:43:37 -0400

--- 00011_¿Quién te entiende?/test..rb
+++ 00011_¿Quién te entiende?/test..rb
@@ -1,13 +1,13 @@
 describe 'Interfaz de' do
   it 'Mercedes' do
-    expect(interfaz_mercedes).to eq ['cantar!']
+    expect(interfaz_mercedes).to match_array ['cantar!']
   end
 
   it 'Pepita' do
-    expect(interfaz_pepita).to eq ['energia', 'cantar!', 'comer_lombriz!', 'volar_en_circulos!']
+    expect(interfaz_pepita).to match_array ['energia', 'cantar!', 'comer_lombriz!', 'volar_en_circulos!']
   end
 
   it 'Norita' do
-    expect(interfaz_norita).to eq ['cantar!', 'comer_lombriz!', 'volar_en_circulos!']
+    expect(interfaz_norita).to match_array ['cantar!', 'comer_lombriz!', 'volar_en_circulos!']
   end  
 end
